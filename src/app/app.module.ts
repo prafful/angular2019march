373ae9c34e1af68ba6157cf0330408c8f7e20519
unknown <prraful@gmail.com>
2019-03-04 17:52:39 +0530

    day 1 with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FatherComponent } from './family/father/father.component';
+import { SonComponent } from './family/son/son.component';
+import { DaughterComponent } from './family/daughter/daughter.component';
+import { FordirectiveComponent } from './directive/fordirective/fordirective.component';
+import { NumbersortPipe } from './custompipe/numbersort.pipe';
+import { StringsortPipe } from './custompipe/stringsort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FatherComponent,
+    SonComponent,
+    DaughterComponent,
+    FordirectiveComponent,
+    NumbersortPipe,
+    StringsortPipe
   ],
   imports: [
     BrowserModule,
